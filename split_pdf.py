25dfdbe8fb9c9f5c9a1ce9a722189503b84e7c06
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-03-27 21:37:50 +0800

    Add cut in one file option

--- split_pdf.py
+++ split_pdf.py
@@ -1,13 +1,12 @@
 """a UI tool to cut pdfs"""
 
 
-from tkinter import *  
+from tkinter import Tk, Label, TOP, Button, Checkbutton, StringVar, BooleanVar, Entry, mainloop
 from tkinter import messagebox
 from tkinter.simpledialog import askstring
 from tkinter.filedialog import askopenfilename, askdirectory
 import os
 from PyPDF2 import PdfWriter, PdfReader
-
 
 
 lablefont = ("Helvetica", 15)
@@ -27,32 +26,39 @@ class ImGUI:
 		self.name = '_part_01'
 		self.text1 = StringVar()
 		self.text1.set(f'Current file: \n{self.fileaddress if self.fileaddress else "not selected"}')
-		self.label1 = Label(self.root, textvariable=self.text1, font = lablefont)
-		self.label1.pack(side=TOP,pady=10)
-		self.button1 = Button(text="Select file", height = 2, width = 10, command = self.getfilename)
-		self.button1.pack(side = TOP, pady = 10)
+		self.label1 = Label(self.root, textvariable=self.text1, font=lablefont)
+		self.label1.pack(side=TOP, pady=10)
+		self.button1 = Button(text="Select file", height=2, width=10, command=self.getfilename)
+		self.button1.pack(side=TOP, pady=10)
 		self.text2 = StringVar()
 		self.text2.set("Save to: \n"+self.saveaddress)
-		self.label2 = Label(self.root, textvariable=self.text2, font = buttonfont)
+		self.label2 = Label(self.root, textvariable=self.text2, font=buttonfont)
 		self.label2.pack(side=TOP, pady=10)
-		self.button2 = Button(text="Save to", height = 2, width = 10, command = self.change_save_address)
-		self.button2.pack(side = TOP, pady = 10)
+		self.button2 = Button(text="Save to", height=2, width=10, command=self.change_save_address)
+		self.button2.pack(side=TOP, pady=10)
 
 		self.text5 = StringVar()
 		self.text5.set(f"Filename: \n{self.name}.pdf")
-		self.label5 = Label(self.root, textvariable=self.text5, font = buttonfont)
+		self.label5 = Label(self.root, textvariable=self.text5, font=buttonfont)
 		self.label5.pack(side=TOP, pady=10)
-		self.button5 = Button(text="Rename", height = 2, width = 10, command = self.rename)
-		self.button5.pack(side = TOP, pady = 10)
+		self.button5 = Button(text="Rename", height=2, width=10, command=self.rename)
+		self.button5.pack(side=TOP, pady=10)
 
-		self.label3 = Label(self.root, font = buttonfont)
+		self.label3 = Label(self.root, font=buttonfont)
 		self.label3.pack(side=TOP)
 
-		self.button3 = Button(text="Add more", height = 2, width = 10, command = self.add_cut_pages)
-		self.button3.pack(side = TOP, pady = 10)
-	
-		self.button4 = Button(text="CUT", height = 2, width = 25, font = lablefont,  command = self.cutme)
-		self.button4.pack(side = TOP, pady = 10)
+		self.button3 = Button(text="Add more", height=2, width=10, command=self.add_cut_pages)
+		self.button3.pack(side=TOP, pady=10)
+
+		self.one_file = BooleanVar()
+		self.one_file.set(True)
+		self.one_file_cb = Checkbutton(self.root, text="Save all pages in one file", variable=self.one_file,
+										command=self.toggle_one_file)
+		self.one_file_cb.pack(side=TOP, pady=10)
+
+		self.button4 = Button(text="CUT", height=2, width=25, font=lablefont,  command=self.cutme)
+		self.button4.pack(side=TOP, pady=10)
+
 		self.add_cut_pages()
 		mainloop()
 
@@ -63,11 +69,12 @@ class ImGUI:
 			self.text5.set("Filename: \n"+self.name+'.pdf')
 		print()
 
-
 	def getfilename(self):
 		self.fileaddress = askopenfilename()
 		self.text1.set('Current file: \n'+self.fileaddress)
-		
+
+	def toggle_one_file(self):
+		self.one_file = not self.one_file
 
 	def change_save_address(self):
 		self.saveaddress = askdirectory()
@@ -75,87 +82,101 @@ class ImGUI:
 		print(self.saveaddress)
 
 	def add_cut_pages(self):
-		self.cutrow+=1
+		self.cutrow += 1
 		data = [(entry[0].get(), entry[1].get()) for entry in self.entries]
 
-
 		for x in range(self.cutrow):
-			
 			labelfrom = Label(self.label3, text='from page: ')
 			labelto = Label(self.label3, text='to page: ')
 			e1 = Entry(self.label3)
 			e2 = Entry(self.label3)
-			
 
-			print('DATA',data[x] if len(data)>x else 'NONE')
-			mytext = data[x] if len(data) > x else ('','')
+			print('DATA', data[x] if len(data) > x else 'NONE')
 
-			e1.insert(0,mytext[0])
-			e2.insert(0,mytext[1])
+			mytext = data[x] if len(data) > x else ('', '')
+			e1.insert(0, mytext[0])
+			e2.insert(0, mytext[1])
 
-		
 			labelfrom.grid(row=x, column=0)
 			labelto.grid(row=x, column=2)
-			e1.grid(row=x,column=1)
-			e2.grid(row=x,column=3)
+			e1.grid(row=x, column=1)
+			e2.grid(row=x, column=3)
 
 			if len(self.entries) > x:
-				self.entries[x] = (e1,e2)
-			else: self.entries.append((e1,e2))
+				self.entries[x] = (e1, e2)
+			else:
+				self.entries.append((e1, e2))
 		
 		self.label3.pack(side=TOP)
-
-
 
 	def clean_data(self, max_pages):
 		checked = []
 		for pair in self.entries:
-			num1,num2 = pair[0].get(), pair[1].get()
+			num1, num2 = pair[0].get(), pair[1].get()
 			if num1.isdigit() and num2.isdigit():
 				try:
 					num1 = min(max(int(num1), 1), max_pages)
 					num2 = min(max(int(num2), 1), max_pages)
-					data = (num1,num2) if num2 > num1 else (num2, num1)
+					data = (num1, num2) if num2 > num1 else (num2, num1)
 					checked.append(data)
-				except:
+				except ValueError:
 					messagebox.showerror(title='Wrong input', message="Only digits allowed")
 					return
 			else: 
 				continue
 		return checked
 
-
-
-
 	def cutme(self):
 		try:
 			print("FILEADDRESS: ", self.fileaddress)
 			mypdf = PdfReader(open(self.fileaddress, "rb"))
 
-
-		except FileNotFoundError: 
+		except FileNotFoundError:
 			messagebox.showerror(title='File not found', message="Please select a file to work with")
 			
 		else:	
 			pages = self.clean_data(len(mypdf.pages))
-			if not pages: return
-			print(pages)
-
-			counter = 1
-			for pair in pages:
-				name = f'{self.saveaddress}\\{self.name}_part_{counter}.pdf' if len(pages)>1 else f'{self.saveaddress}\\{self.name}.pdf'
-				print('MYNAME', name)
-				my_file=open(name, "wb")
-				writer = PdfWriter()
-
-				for page_num in range(pair[0]-1,pair[1]):
-					print(page_num)
-					page = mypdf.pages[page_num]
-					writer.add_page(page)
-
-				counter += 1
-				writer.write(my_file)
+			if not pages:
+				return
+			# print(pages)
+			if self.one_file:
+				self.cut_in_one(mypdf, pages)
+			else:
+				self.cut_many(mypdf, pages)
 			messagebox.showinfo('Done', "Files cut successfully")
+
+	def cut_in_one(self, mypdf, pages):
+		print('Cut all in one page')
+		# Save all sets of pages in one file
+		name = f'{self.saveaddress}\\{self.name}.pdf'
+		print('MYNAME', name)
+		my_file = open(name, "wb")
+		writer = PdfWriter()
+
+		for pair in pages:
+			for page_num in range(pair[0] - 1, pair[1]):
+				print(page_num)
+				page = mypdf.pages[page_num]
+				writer.add_page(page)
+
+		writer.write(my_file)
+
+	def cut_many(self, mypdf, pages):
+		counter = 1
+		for pair in pages:
+			name = f'{self.saveaddress}\\{self.name}_part_{counter}.pdf' if len(pages) > 1 else \
+					f'{self.saveaddress}\\{self.name}.pdf'
+			print('MYNAME', name)
+			my_file = open(name, "wb")
+			writer = PdfWriter()
+
+			for page_num in range(pair[0]-1, pair[1]):
+				print(page_num)
+				page = mypdf.pages[page_num]
+				writer.add_page(page)
+
+			counter += 1
+			writer.write(my_file)
 
 
 a = ImGUI()
